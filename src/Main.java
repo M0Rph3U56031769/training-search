c6ec9cad88b74774ba8d0359b6fd78ead76ed882
Daniel Nagy <nagydaniel1337@gmail.com>
2019-05-30 17:54:39 +0200

    replace ',' to '-' in video titles (for proper csv handling)

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -25,13 +24,28 @@ public class Main {
         vidi.click();
 
 //        Collecting the 3 training links
+        WebElement firstCourseName = driver.findElement(By.cssSelector("div.g:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1) > h3:nth-child(1)"));
+        String name1 = firstCourseName.getText();
+        String name1Converted = name1.replace(',','-');
+        System.out.println(name1Converted);
+
         WebElement firstcourse = driver.findElement(By.cssSelector("div.g:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1)"));
         String link1 = firstcourse.getAttribute("href");
         System.out.println(link1);
 
+        WebElement secondCourseName = driver.findElement(By.cssSelector("div.g:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1) > h3:nth-child(1)"));
+        String name2 = secondCourseName.getText();
+        String name2Converted = name2.replace(',','-');
+        System.out.println(name2Converted);
+
         WebElement secondcourse = driver.findElement(By.cssSelector("div.g:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1)"));
         String link2 = secondcourse.getAttribute("href");
         System.out.println(link2);
+
+        WebElement thirdCourseName = driver.findElement(By.cssSelector("div.g:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1) > h3:nth-child(1)"));
+        String name3 = thirdCourseName.getText();
+        String name3Converted = name3.replace(',','-');
+        System.out.println(name3Converted);
 
         WebElement thirdcourse = driver.findElement(By.cssSelector("div.g:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1)"));
         String link3 = thirdcourse.getAttribute("href");
@@ -43,7 +57,7 @@ public class Main {
         String csvFile = "links.csv";
         Writer fileWriter = new FileWriter(csvFile);
 
-        fileWriter.write(link1+"\n"+link2+"\n"+link3);
+        fileWriter.write("name,link\n"+name1Converted+","+link1+"\n"+name2Converted+","+link2+"\n"+name3Converted+","+link3);
         fileWriter.close();
 
     }

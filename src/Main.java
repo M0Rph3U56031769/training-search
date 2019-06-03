647f539d198413cad9fe28bed68714e54530fb67
Daniel Nagy <nagydaniel1337@gmail.com>
2019-06-03 13:13:37 +0200

    encapsulation

--- src/Main.java
+++ src/Main.java
@@ -11,84 +11,47 @@ public class Main {
 
         ArrayList<String> names = new ArrayList<>();
         ArrayList<String> links = new ArrayList<>();
-        String training_text;
-        String searchengine_url;
-        String google_input_field;
-
-        String google_search_button;
-        String google_category_videos;
+//        String training_text;
+//        String searchengine_url;
+//        String google_input_field;
+//
+//        String google_search_button;
+//        String google_category_videos;
         int name_counter=1;
         int link_counter=1;
+        int course_amount=3;
 
         Google google_properties = new Google();
 
-        searchengine_url = google_properties.getGoogle_url();
-        google_input_field = google_properties.getInput_field();
-        training_text = google_properties.getTraining_text();
-        google_search_button = google_properties.getGoogle_search_button();
-        google_category_videos = google_properties.getGoogle_category_videos();
+//        searchengine_url = google_properties.getGoogle_url();
+//        google_input_field = google_properties.getInput_field();
+//        training_text = google_properties.getTraining_text();
+//        google_search_button = google_properties.getGoogle_search_button();
+//        google_category_videos = google_properties.getGoogle_category_videos();
         ArrayList<Object> google_searchresult_name = new ArrayList<Object>(google_properties.getGoogle_searchresult_name());
         ArrayList<Object> google_searchresult_link = new ArrayList<Object>(google_properties.getGoogle_searchresult_link());
 
+//        Init webdriver
+        FirefoxDriver browser = google_properties.initBrowser();
 
-        FirefoxDriver driver=new FirefoxDriver();
-
-        driver.get(searchengine_url);
-
-
-//        fill the input field
-        WebElement element=driver.findElement(By.cssSelector(google_input_field));
-        element.sendKeys(training_text);
-
-//        click on search button
-        WebElement button=driver.findElement(By.cssSelector(google_search_button));
-        button.click();
-
-//        switch to videos
-        WebElement vidi = driver.findElement(By.cssSelector(google_category_videos));
-        vidi.click();
 
 //        Collecting the 3 training links
-        WebElement firstCourseName = driver.findElement(By.cssSelector(google_searchresult_name.get(0) + Integer.toString(name_counter) + google_searchresult_name.get(1)));
-        String name1 = firstCourseName.getText();
-        String name1Converted = name1.replace(',','-');
-        System.out.println(name1Converted);
-        names.add(name1Converted);
-        name_counter++;
+        for (int i=0; i<course_amount; i++){
+            WebElement firstCourseName = browser.findElement(By.cssSelector(google_searchresult_name.get(0) + Integer.toString(name_counter) + google_searchresult_name.get(1)));
+            String name1 = firstCourseName.getText();
+            String name1Converted = name1.replace(',','-');
+            System.out.println(name1Converted);
+            names.add(name1Converted);
+            name_counter++;
 
-        WebElement firstCourseLink = driver.findElement(By.cssSelector(google_searchresult_link.get(0) + Integer.toString(link_counter) + google_searchresult_link.get(1)));
-        String link1 = firstCourseLink.getAttribute("href");
-        System.out.println(link1);
-        links.add(link1);
-        link_counter++;
+            WebElement firstCourseLink = browser.findElement(By.cssSelector(google_searchresult_link.get(0) + Integer.toString(link_counter) + google_searchresult_link.get(1)));
+            String link1 = firstCourseLink.getAttribute("href");
+            System.out.println(link1);
+            links.add(link1);
+            link_counter++;
+        }
 
-        WebElement secondCourseName = driver.findElement(By.cssSelector(google_searchresult_name.get(0) + Integer.toString(name_counter) + google_searchresult_name.get(1)));
-        String name2 = secondCourseName.getText();
-        String name2Converted = name2.replace(',','-');
-        System.out.println(name2Converted);
-        names.add(name2Converted);
-        name_counter++;
-
-
-        WebElement secondcourse = driver.findElement(By.cssSelector(google_searchresult_link.get(0) + Integer.toString(link_counter) + google_searchresult_link.get(1)));
-        String link2 = secondcourse.getAttribute("href");
-        System.out.println(link2);
-        links.add(link2);
-        name_counter++;
-
-        WebElement thirdCourseName = driver.findElement(By.cssSelector(google_searchresult_name.get(0) + Integer.toString(name_counter) + google_searchresult_name.get(1)));
-        String name3 = thirdCourseName.getText();
-        String name3Converted = name3.replace(',','-');
-        System.out.println(name3Converted);
-        names.add(name3Converted);
-
-        WebElement thirdcourse = driver.findElement(By.cssSelector(google_searchresult_link.get(0) + Integer.toString(link_counter) + google_searchresult_link.get(1)));
-        String link3 = thirdcourse.getAttribute("href");
-        System.out.println(link3);
-        links.add(link3);
-
-
-        driver.quit();
+        browser.quit();
 
         Csvhandler toFile = new Csvhandler();
         toFile.csvhandler(names, links);
--- src/searchengines/Google.java
+++ src/searchengines/Google.java
@@ -1,7 +1,13 @@
 package searchengines;
 
 import com.opencsv.CSVReader;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
@@ -15,12 +21,15 @@ public class Google {
     private String google_category_videos;
     private List<String> google_searchresult_name;
     private List<String> google_searchresult_link;
+    private String google_csv="\\src\\searchengines\\google.csv";
 
 
     public Google()throws IOException {
+        String cwd = new File("").getAbsolutePath();
+        String google_csv = cwd+this.google_csv;
 
 //        Read CSV from file
-        try (CSVReader csvReader = new CSVReader(new FileReader("C:\\Users\\Daniel_Nagy1\\IdeaProjects\\sel02\\src\\searchengines\\google.csv"))) {
+        try (CSVReader csvReader = new CSVReader(new FileReader(google_csv))) {
 
             List<String> url_elements = new ArrayList<>(Arrays.asList(csvReader.readNext()));
             this.google_url = url_elements.get(1);
@@ -38,14 +47,53 @@ public class Google {
 
     }
 
-    public String getGoogle_url(){ return this.google_url; }
-    public String getTraining_text(){
+    @Contract(pure = true)
+    private String getGoogle_url(){ return this.google_url; }
+    @Contract(pure = true)
+    private String getTraining_text(){
         return this.training_text;
     }
-    public String getInput_field() { return this.input_field; }
-    public String getGoogle_search_button() {return this.google_search_button; }
-    public String getGoogle_category_videos() { return this.google_category_videos; }
+    @Contract(pure = true)
+    private String getInput_field() { return this.input_field; }
+    @Contract(pure = true)
+    private String getGoogle_search_button() {return this.google_search_button; }
+    @Contract(pure = true)
+    private String getGoogle_category_videos() { return this.google_category_videos; }
     public List getGoogle_searchresult_name() { return google_searchresult_name; }
     public List getGoogle_searchresult_link() { return google_searchresult_link; }
+
+
+    public FirefoxDriver initBrowser(){
+        FirefoxDriver driver=new FirefoxDriver();
+        this.goStartPage(driver);
+        this.fillInputField(driver);
+        this.clickSearchButton(driver);
+        this.switchCategoryToVideo(driver);
+        return driver;
+    }
+
+    @Contract("_ -> param1")
+    private FirefoxDriver goStartPage(@NotNull FirefoxDriver browser){
+        browser.get(this.getGoogle_url());
+        return browser;
+    }
+    @Contract("_ -> param1")
+    private FirefoxDriver fillInputField(@NotNull FirefoxDriver browser) {
+        WebElement element=browser.findElement(By.cssSelector(this.getInput_field()));
+        element.sendKeys(this.getTraining_text());
+        return browser;
+    }
+    @Contract("_ -> param1")
+    private FirefoxDriver clickSearchButton(@NotNull FirefoxDriver browser){
+        WebElement button=browser.findElement(By.cssSelector(this.getGoogle_search_button()));
+        button.click();
+        return browser;
+    }
+    @Contract("_ -> param1")
+    private FirefoxDriver switchCategoryToVideo(@NotNull FirefoxDriver browser){
+        WebElement vidi = browser.findElement(By.cssSelector(this.getGoogle_category_videos()));
+        vidi.click();
+        return browser;
+    }
 
 }
